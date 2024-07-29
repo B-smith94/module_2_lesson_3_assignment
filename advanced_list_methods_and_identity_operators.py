790c28273ce2796f8bc9d37fe68b9ec96e73f7c5
b-smith94 <byrondsmith9@gmail.com>
2024-07-29 14:58:44 -0400

    finished numbers 2 and 3, started number 1

---
+++ advanced_list_methods_and_identity_operators.py
@@ -0,0 +1,9 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+submitted.remove("Alice")
+
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice submitted her assignment and attended class.")
+else:
+    print("Alice either forgot to submit her assignment or missed class.")
---
+++ python_list_transformation.py
@@ -0,0 +1,9 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+grades.sort()
+grades.reverse()
+print(grades)
+
+grade_average = grades[0:] // len(grades)
+
+print(grade_average)
